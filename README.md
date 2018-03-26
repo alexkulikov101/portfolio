58f9e131df6751bb85fdb8d2a0233fdeba6697b4
kulinar310 <bullet_310@mail.ru>
2018-03-26 14:22:07 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# kulinar310.github.io
---
+++ js/script.js
@@ -0,0 +1,10 @@
+/*$('#exampleModalCenter').on('show.bs.modal', function (event) {
+  // получить кнопку, которая его открыло
+  var button = $(event.relatedTarget)
+  // извлечь информацию из атрибута data-content
+  var content = button.data('content')
+  // вывести эту информацию в элемент, имеющий id="content"
+  $(this).find('#content').text(content);
+})*/
+
+
